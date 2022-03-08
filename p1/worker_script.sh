0fef2a0af233648473d33301467408a6391d7e8d
rtam amal <amalrtam@gmail.com>
2022-03-08 20:52:24 +0100

    to fix => agent ip not working
    Signed-off-by: rtam amal <amalrtam@gmail.com>

--- p1/worker_script.sh
+++ p1/worker_script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # curl -sfL https://get.k3s.io | sh -s - --server "https://192.168.42.110:6443" --write-kubeconfig-mode 644 --flannel-iface=eth1 --token-file "/vagrant_shared/token" --kubeconfig="/vagrant_shared/k3s.yaml"
-curl -sfL https://get.k3s.io | KUBECONFIG=/vagrant_shared/k3s.yaml K3S_URL=https://192.168.42.110:6443 K3S_TOKEN_FILE=/vagrant_shared/token sh -
+echo "export KUBECONFIG=/vagrant_shared/k3s.yaml" >> .bashrc
+echo "export K3S_URL=https://192.168.42.110:6443" >> .bashrc
+echo "export K3S_TOKEN_FILE=/vagrant_shared/token" >> .bashrc
+source .bashrc
+curl -sfL https://get.k3s.io | KUBECONFIG=$KUBECONFIG K3S_URL=$K3S_URL K3S_TOKEN_FILE=$K3S_TOKEN_FILE sh - --flannel-iface=eth1
