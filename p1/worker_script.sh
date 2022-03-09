8a49a056290601ad23c74ad02c04e330b576bf28
alizaynoune <zaynoune.ali@gmail.com>
2022-03-09 16:46:43 +0100

    fix P1
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- p1/worker_script.sh
+++ p1/worker_script.sh
@@ -4,4 +4,5 @@ echo "export KUBECONFIG=/vagrant_shared/k3s.yaml" >> .bashrc
 echo "export K3S_URL=https://192.168.42.110:6443" >> .bashrc
 echo "export K3S_TOKEN_FILE=/vagrant_shared/token" >> .bashrc
 source .bashrc
-curl -sfL https://get.k3s.io | KUBECONFIG=$KUBECONFIG K3S_URL=$K3S_URL K3S_TOKEN_FILE=$K3S_TOKEN_FILE sh - --flannel-iface=eth1
+# curl -sfL https://get.k3s.io | KUBECONFIG=$KUBECONFIG K3S_URL=$K3S_URL K3S_TOKEN_FILE=$K3S_TOKEN_FILE sh - --flannel-iface eth1
+curl -sfL https://get.k3s.io | K3S_URL=$K3S_URL K3S_TOKEN_FILE=$K3S_TOKEN_FILE  sh -s - --flannel-iface eth1
