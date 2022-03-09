070cda4f5a6d0bd90f6f66ab06a24c878203d41f
alizaynoune <zaynoune.ali@gmail.com>
2022-03-09 20:53:46 +0100

    chi 7aja
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- p1/worker_script.sh
+++ p1/worker_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # curl -sfL https://get.k3s.io | sh -s - --server "https://192.168.42.110:6443" --write-kubeconfig-mode 644 --flannel-iface=eth1 --token-file "/vagrant_shared/token" --kubeconfig="/vagrant_shared/k3s.yaml"
-echo "export KUBECONFIG=/vagrant_shared/k3s.yaml" >> .bashrc
+# echo "export KUBECONFIG=/vagrant_shared/k3s.yaml" >> .bashrc
 echo "export K3S_URL=https://192.168.42.110:6443" >> .bashrc
 echo "export K3S_TOKEN_FILE=/vagrant_shared/token" >> .bashrc
 source .bashrc
