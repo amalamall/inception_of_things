52f273fe6aabdf54e419f276302a13915057e254
rtam amal <amalrtam@gmail.com>
2022-03-14 18:02:02 +0100

    remove bind adress

--- p1/scripts/server_script.sh
+++ p1/scripts/server_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 systemctl disable firewalld --now
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address=192.168.42.110 --flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -s -
 cp /var/lib/rancher/k3s/server/token /vagrant_shared
 # cp /etc/rancher/k3s/k3s.yaml /vagrant_shared
