72094a655280dac188e808bf43f544ca56e330b3
alizaynoune <zaynoune.ali@gmail.com>
2022-03-21 17:56:02 +0100

    fix script_PKG
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- p3/scripts/install_PKG.sh
+++ p3/scripts/install_PKG.sh
@@ -4,20 +4,21 @@
 apt-get update && apt upgrade -y
 # install docker
 # set up repository docker
-apt-get install ca-certificates curl gnupg lsb-release
+apt-get install ca-certificates curl gnupg lsb-release -y
 # add docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # update
-apt-get update
+apt-get update && apt upgrade -y
 # install docker
-apt-get install docker-ce docker-ce-cli containerd.io
+snap install docker
+# apt-get install docker-ce docker-ce-cli containerd.io -y
 
 # install k3d
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash 
 
 # install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl 
