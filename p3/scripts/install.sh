0a3ad71e79e358ed75386376d52a3648de21e7f9
rtam amal <amalrtam@gmail.com>
2022-03-24 14:58:40 +0100

    intall apache2-utils
    Signed-off-by: rtam amal <amalrtam@gmail.com>

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -20,4 +20,6 @@ curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 # install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl 
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# install aptache2-utils
+apt install apache2-utils 
