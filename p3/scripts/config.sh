c506f8cd376bdee3fca517ba6046a29d7f4f3b82
alizaynoune <zaynoune.ali@gmail.com>
2022-03-28 13:33:33 +0000

    fix p3/config.sh
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- p3/scripts/config.sh
+++ p3/scripts/config.sh
@@ -3,13 +3,10 @@ k3d cluster create my-cluster --api-port 6443 -p 8080:80@loadbalancer --port 844
 kubectl create namespace argocd
 kubectl create namespace dev
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-#curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-#chmod +x /usr/local/bin/argocd
-#kubectl port-forward svc/argocd-server -n argocd 8080:443 --address='0.0.0.0' > /dev/null 0>&1 &
 ARGOCDINITIALPASS=1337@LEET
 kubectl patch secret   -n argocd argocd-secret  -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" ${ARGOCDINITIALPASS} | tr -d ':\n')'"}}'
 kubectl patch deploy argocd-server \
     -n argocd \
     -p '[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--insecure"}]' \
     --type json
-kubectl apply -f $PWD/../confs/
+kubectl apply -f $(dirname "$0" | awk 'BEGIN{FS=OFS="/"}NF--')"/confs" -n argocd
