a153799a3af156a33f2bbb654d9eb1abe44c6682
rtam amal <amalrtam@gmail.com>
2022-04-11 17:46:09 +0000

    update bonus/scripts.sh
    Signed-off-by: rtam amal <amalrtam@gmail.com>

--- bonus/scripts/config.sh
+++ bonus/scripts/config.sh
@@ -2,6 +2,7 @@
 sudo k3d cluster create my-cluster --api-port 6443 -p 8080:80@loadbalancer --port 8443:443@loadbalancer  --port 8888:8888@loadbalancer
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
+sudo kubectl create namespace gitlab
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 ARGOCDINITIALPASS=1337@LEET
 sudo kubectl patch secret   -n argocd argocd-secret  -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" ${ARGOCDINITIALPASS} | tr -d ':\n')'"}}'
@@ -10,3 +11,4 @@ sudo kubectl patch deploy argocd-server \
     -p '[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--insecure"}]' \
     --type json
 #sudo kubectl apply -f $(dirname "$0" | awk 'BEGIN{FS=OFS="/"}NF--')"/confs" -n argocd
+#sudo kubectl exec --namespace gitlab -it gitlab-deployment-77bd9b5759-kb4ts -- /bin/sh
