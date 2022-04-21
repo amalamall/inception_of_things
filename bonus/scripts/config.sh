a123033c4a264bc1f4ca0039dd5d50f1f268b22c
alizaynoune <zaynoune.ali@gmail.com>
2022-04-21 17:42:02 +0000

    bonus done
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- bonus/scripts/config.sh
+++ bonus/scripts/config.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-sudo k3d cluster create my-cluster --api-port 6443 -p 8080:80@loadbalancer -p 8443:443@loadbalancer  -p 8888:8888@loadbalancer -p 9000:9000@loadbalancer
+sudo k3d cluster create my-cluster --api-port 6443  -p 8080:80@loadbalancer --port 8443:443@loadbalancer  --port 8888:8888@loadbalancer -p 80:8181@loadbalancer -p 31337:31337@loadbalancer
+
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
 sudo kubectl create namespace gitlab
+
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
+sudo helm install --wait -n gitlab gitlab gitlab/gitlab -f /confs/gitlab/values.yaml
+sudo kubectl patch svc gitlab-webservice-default -p '{"spec": {"type": "LoadBalancer"}}' -n gitlab
+
+
+
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 ARGOCDINITIALPASS=1337@LEET
 sudo kubectl patch secret   -n argocd argocd-secret  -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" ${ARGOCDINITIALPASS} | tr -d ':\n')'"}}'
@@ -10,7 +19,14 @@ sudo kubectl patch deploy argocd-server \
     -n argocd \
     -p '[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--insecure"}]' \
     --type json
-sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2
-sudo kubectl apply -f $(dirname "$0" | awk 'BEGIN{FS=OFS="/"}NF--')"/confs" -n argocd
-sudo kubectl apply -f $(dirname "$0" | awk 'BEGIN{FS=OFS="/"}NF--')"/confs/gitlab" -n gitlab
-#sudo kubectl exec --namespace gitlab -it gitlab-deployment-77bd9b5759-kb4ts -- /bin/bash
+
+
+sudo kubectl apply -f $(dirname "$0" | awk 'BEGIN{FS=OFS="/"}NF--')"/confs/argocd" -n argocd
+
+
+
+echo "================= Admin Passowrd ================="
+sudo kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' -n gitlab | base64 --decode ; echo
+
+
+
--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ mkdir $HOME/goinfre/VirtualBox\ VMs $HOME/goinfre/.vagrant.d
 ln -s $HOME/goinfre/.vagrant.d $HOME/.vagrant.d
 ln -s $HOME/goinfre/VirtualBox\ VMs $HOME/VirtualBox\ VMs
 
-cd $HOME/goinfre
-curl -O https://mirror.marwan.ma/ubuntu-releases/20.04.4/ubuntu-20.04.4-live-server-amd64.iso
+# cd $HOME/goinfre
+# curl -O https://mirror.marwan.ma/ubuntu-releases/20.04.4/ubuntu-20.04.4-live-server-amd64.iso
